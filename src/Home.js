687757ba8c1ec95dcadfab96f48f6854b9ba88a1
Chandu reddy <50436294+chandureddy04@users.noreply.github.com>
2025-04-26 15:47:04 +0530

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -12,7 +12,7 @@ function Home() {
             <div className='home__title'>
                 <h1>Hi,</h1>
                 <h1>I'm <span>C</span>handu,</h1>
-                <h1>web deveoper</h1>
+                <h1>web developer</h1>
             </div>
             <p>Front End Developer</p>
             <button onClick={()=>console.log('')}>Contact me</button>
